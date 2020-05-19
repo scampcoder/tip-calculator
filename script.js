e20fa50fdc6c16855b37bbefe08cd69c5dd22e41
Victoria Kee <victoriakee95@gmail.com>
2020-05-19 15:54:48 -0500

    Initial JS

---
+++ script.js
@@ -0,0 +1,12 @@
+function calculate(){
+  const amount = $('amount').val(); //amount value
+  const percentage = $('#percentage').val(); //percent value
+  const tip = amount * (percentage / 100); //tip calculations
+  const total = Number(amount) + tip; //add for total
+
+  $('#tip').val( tip.toFixed(2) ); //decimal pts 2
+    $('#total').val( total.toFixed(2) ); //decimal pts 2
+
+    return false; //prevent default behavior/don't load new pg
+  }
+  $('#calculator').submit( calculate ); //when submit button clicked, run calculate()
